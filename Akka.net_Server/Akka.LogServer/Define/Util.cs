98471e58399fb9cf13a7efe5cadd069f749fdc8d
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2025-01-21 08:10:06 +0900

    protobuf bin 폴더 추가

--- Akka.net_Server/Akka.LogServer/Define/Util.cs
+++ Akka.net_Server/Akka.LogServer/Define/Util.cs
@@ -8,13 +8,10 @@ namespace Akka.LogServer
 {
     public static class Util
     {
-
         public static string RoomNameing(int roomId)
         {
             string today = DateTime.Now.ToString("yyyyMMdd");
-            Console.WriteLine(today);  // 출력: 20250120
             return $"Room{roomId}_{today}";
         }
-
     }
 }
---
+++ Akka.net_Server/Protobuf/protoc-3.12.3-win64/bin/GenProto.bat
@@ -0,0 +1,12 @@
+pushd %~dp0
+protoc.exe -I=./ --csharp_out=./ ./Protocol.proto 
+protoc.exe -I=./ --csharp_out=./ ./ClusterProtocol.proto 
+IF ERRORLEVEL 1 PAUSE
+
+START ../../../PacketGenerator/bin/Debug/net6.0/PacketGenerator.exe ./Protocol.proto
+XCOPY /Y Protocol.cs "../../../Akka.Protocol.Shared"
+
+XCOPY /Y ClusterProtocol.cs "../../../Akka.Protocol.Shared"
+
+XCOPY /Y ClientPacketManager.cs "../../../DummyClient/Packet"
+XCOPY /Y ServerPacketManager.cs "../../../Akka.Server/Packet"
